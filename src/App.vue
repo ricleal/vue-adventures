b45f195e8e850c59074532027ad044163290bee8
Alan B. Carpenter <uac@studentbox.ornl.gov>
2018-06-19 16:02:57 -0400

    three components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
+    <Goals />
+    <Graph />    
     <Skills />
+    
   </div>
 </template>
 
 <script>
 
-import Skills from './components/Skills.vue'
+import Goals from './components/Goals.vue'
+import Graph from './components/Graph.vue'
+import Skills from './components/Skills.vue' 
 
 export default {
   name: 'app',
   components: {
+    Goals,
+    Graph,
     Skills
+
   }
 }
 
