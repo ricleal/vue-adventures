efcec4be7516eaa46370445b491465af047be92f
Ricardo Ferraz Leal <ricleal@gmail.com>
2018-06-22 18:23:21 -0400

    Added mathjs

--- src/components/Tools.vue
+++ src/components/Tools.vue
@@ -21,33 +21,23 @@
         </v-layout>
         <v-layout row>
             <v-flex xs8>
-                <v-text-field name="arange" label="Arange" v-model.number="arange" disabled></v-text-field>
+                <v-text-field name="arange" label="X" v-model.number="arange" disabled></v-text-field>
             </v-flex>
         </v-layout>
         <v-layout row>
             <v-flex xs8>
-                <v-btn v-on:click="calculateY" color="info">Calulate Y</v-btn>
+                <v-text-field id="equation" name="equation" label="Equation" :rules="[rules.required, rules.equation]"
+                 v-model.number="equation"></v-text-field>
             </v-flex>
         </v-layout>
-				<v-layout row>
+        <v-layout row>
             <v-flex xs8>
-                <v-text-field name="y" label="y" v-model.number="y" disabled></v-text-field>
+                <v-btn v-on:click="calculateY" :disabled="this.equation === null" color="info">Calulate Y</v-btn>
             </v-flex>
         </v-layout>
-    </v-container>
-
-    <v-container grid-list-md>
-        <v-layout row wrap>
-            <v-flex xs12>
-                <v-card>
-                    <v-card-title primary-title>
-                        <div>
-                            <h3 class="headline mb-0">ARange</h3>
-														<div>X:  {{arange}}</div> 
-														<div>Y:  {{y}}</div> 
-                        </div>
-                    </v-card-title>
-                </v-card>
+        <v-layout row>
+            <v-flex xs8>
+                <v-text-field name="y" label="Y" v-model.number="y" disabled></v-text-field>
             </v-flex>
         </v-layout>
     </v-container>
@@ -56,7 +46,12 @@
 </template>
 
 <script>
-import { EventBus } from '../main.js';
+
+const math = require('mathjs')
+
+import {
+    EventBus
+} from '../main.js';
 
 export default {
     name: "tools",
@@ -64,18 +59,27 @@ export default {
         valid: false,
         from: 1,
         to: 100,
-				steps: 100,
-				y: null
-		}),
-		methods: {
-			calculateY: function (event) {
-				if (event) {
-					console.log(event);
-				}
-				this.y = this.arange.map(x => x * 2);
-				EventBus.$emit('tools-y', this.y);
-			}
-		},
+        steps: 100,
+        y: null,
+        equation: null,
+        rules: {
+          required: (value) => !!value || 'Required.',
+          equation: (value) => {
+            return  value.includes('x') || 'You must include x'
+          }
+        }
+    }),
+    methods: {
+        calculateY: function (event) {
+          console.log(event);
+          let scope = {
+            x: this.arange
+          }
+          this.y = math.eval(this.equation, scope);
+
+          EventBus.$emit('tools-y', this.y);
+        }
+    },
     computed: {
         arange: function () {
             if (this.to && this.from && this.steps) {
@@ -85,13 +89,13 @@ export default {
                 for (i = this.from; i <= this.to; i += increment) {
                     result.push(parseFloat(i.toFixed(2)));
                 }
-								//console.log(result);
-								EventBus.$emit('tools-x', result);
+                //console.log(result);
+                EventBus.$emit('tools-x', result);
                 return result;
             }
         }
-		}
-		
+    }
+
 };
 </script>
 
